9d10e7606edad2c412f09fb1e6b9c1f082fa2d56
Çağrı Kılıç <ackwar@Edizs-MacBook-Air.local>
2022-06-11 17:05:39 +0300

    eslint refactoring

--- app/utils/bigquery-utils2.js
+++ app/utils/bigquery-utils2.js
@@ -1,11 +1,12 @@
+/* eslint-disable no-undef */
+const { queryBQ } = require('./bigquery-utils');
 
-const { BigQuery } = require('@google-cloud/bigquery');
-const {queryBQ} = require('./bigquery-utils')
-const bigquery = new BigQuery();
-const query = "test"
-jest.setTimeout(10000)
+const query = 'test';
+jest.setTimeout(10000);
+// Test jest'in timeout süresinden daha uzun sürüyor. Bu sebeple fail oluyor.
+// TO-DO: Çözüm bul
 describe('Should throws', () => {
-    test('return error when BigQuery has errors',async () =>{
-        await expect(queryBQ(query)).rejects.toThrow('Resource not found')
-    })
-})
+  test('return error when BigQuery has errors', async () => {
+    await expect(queryBQ(query)).rejects.toThrow('Resource not found');
+  });
+});
